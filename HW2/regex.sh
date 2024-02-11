2418533015fbf2b184ffde7acfe7a9c5c7f5ff53
Maahir <maahir.vohra14@gmail.com>
2024-02-11 03:05:06 -0500

    HW2 complete

--- HW2/regex.sh
+++ HW2/regex.sh
@@ -1,7 +1,32 @@
+#a
 containsFoo='[Ff]oo'
 
-oneUpper='^[A-Z]'
+#b
+oneUpper='^[A-Z][a-z]+'
 
-emptyLines='( |\n)+'
+#c
+emptyLines='^ *$'
 
-threeSets='([A-Z|a-z]{2}){3}'
+#d
+threeSets='([A-Za-z])\1.*([A-Za-z])\2.*([A-Za-z])\3'
+
+#e
+manyFlavors='fla(v|V)(o|O)(u|U)?(r|r)(i|I)?(n|N)?(g|G)?'
+
+#f
+mineMine='( [Mm]ine|   [Mm]ine){2,}$'
+
+#g
+dateRange='^19([2-9][8-9]|[3-9][0-9])-(0[1-9]|1[0-2])-(1[6-9]|2[0-9]|3[01])$'
+
+#h
+LastFirstAndMiddle='(([A-Z][a-z]+), ([A-Z][a-z]+) ([A-Z][a-z]+)$)|(([A-Z][a-z]+), ([A-Z][a-z]+) [A-Z].$)|(([A-Z][a-z]+), ([A-Z][a-z]+)$)'
+
+#i
+telephoneNumber='^([13579][0-9][0-9])-([0-9][0-9][0-9])-([0-9][0-9][0-9][0-9])$'
+
+#j
+doubleMatchQuoted='".*([A-Za-z].*){1,}"'
+
+#k
+romanNumeral='^(I{0,3}|VI{0,3}|IV|IX|X{0,3}(I{0,3}VI{0,3})?|XXXIX)$'
--- HW2/siri.sh
+++ HW2/siri.sh
@@ -1,18 +1,67 @@
 #!/bin/bash
 
-# Define the constant word
-constant_word="supercalifragilisticexpialidocious"
+inputWord="$1"
 
-# Get the input word from command-line argument
-input_word="$1"
+#false by default
+inWord=1
 
-# Sort both words
-sorted_constant_word=$(echo "$constant_word" | grep -o . | sort | tr -d '\n')
-sorted_input_word=$(echo "$input_word" | grep -o . | sort | tr -d '\n')
+#check if any character is not in supercali...
+inputNotInWord=$(echo $1 | egrep '[^supercalifragilisticexpialidocious]+')
 
-# Check if the input word is a subset of the constant word
-if [[ "$sorted_constant_word" == *"$sorted_input_word"* ]]; then
-    echo "$input_word is composed only of characters from $constant_word"
+#if the length of inputNotInWord = 0, then we know every word in the input exists in supercali...
+#so set inWord to true
+if [ -z $inputNotInWord ]
+then
+    inWord=0
 else
-    echo "$input_word is not composed only of characters from $constant_word"
+    # a letter in the input doesnt exist in supercali.. so just echo 0 and end program
+    echo 0
+    exit
 fi
+
+declare -A superCali
+wordInSuperCali=0
+loopRepeated=0
+
+superCali["i"]=7
+superCali["c"]=3
+superCali["a"]=3
+superCali["s"]=3
+superCali["l"]=3
+superCali["u"]=2
+superCali["p"]=2
+superCali["e"]=2
+superCali["r"]=2
+superCali["o"]=2
+superCali["f"]=1
+superCali["g"]=1
+superCali["t"]=1
+superCali["x"]=1
+superCali["d"]=1
+
+
+# didn't know you can do a for loop like this!
+#loop through each letter while i is less than the length of the inputWord
+#have to assign letter to a slice of the input string (in this case just 1 letter at a time)
+for ((i=0; i < ${#inputWord}; i++))
+do
+    letter=${inputWord:i:1}
+    var=$(echo $letter | egrep '[supercalifragilisticexpialidocious]')
+
+    if [ -n "${superCali[$letter]}" -a "${superCali[$letter]}" -gt 0 ]
+    then
+        ((superCali[$letter]--))
+    else
+        break
+    fi
+
+    ((loopRepeated++))
+
+    if (( i == ${#inputWord}-1 ))
+    then
+        (( wordInSuperCali++ ))
+        i=-1
+    fi
+done
+
+echo $wordInSuperCali
