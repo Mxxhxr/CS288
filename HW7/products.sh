c08e8e2897bda3281f3783fde81406d201cd1fdb
Maahir <maahir.vohra14@gmail.com>
2024-04-23 03:23:23 -0400

    inconsistency in curl and/or tagsoup

--- HW7/products.sh
+++ HW7/products.sh
@@ -1,1 +1,24 @@
-!#bin/bash
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "Invalid file amount"
+    exit 1
+fi
+
+link1=$1
+link2=$2
+current_dir=$(dirname "$0")
+
+# Process links from ex.txt and ex2.txt
+for file in "$link1" "$link2"
+do
+    i=1
+    while IFS= read -r line
+    do
+        echo "Downloading $line"
+        wget -P "$current_dir" -O "file${i}_${file##*_}.html" "$line"
+        java -jar tagsoup-1.2.1.jar --files "file${i}_${file##*_}.html"
+        ((i++))
+    done < "$file"
+done
