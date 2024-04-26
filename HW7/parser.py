72c376137b2425dfd084194757f82ef8c06cfbbc
Maahir <maahir.vohra14@gmail.com>
2024-04-26 04:25:11 -0400

    parser complete and puts in database

--- HW7/parser.py
+++ HW7/parser.py
@@ -1,8 +1,34 @@
 import sys
 from xml.dom import minidom as DOM
-import json
+import mysql.connector
+
 
 xhtmlFile = sys.argv[1]
+
+#all these functions are for description
+def find_element_by_id(element, target_id):
+    if element.nodeType == element.ELEMENT_NODE and element.getAttribute('id') == target_id:
+        return element
+    for child in element.childNodes:
+        found_element = find_element_by_id(child, target_id)
+        if found_element:
+            return found_element
+    return None
+
+def get_text_content(element, start=False):
+    text_content = ""
+    for child in element.childNodes:
+        if child.nodeType == child.TEXT_NODE:
+            if start:
+                text_content += child.data
+        elif child.nodeType == child.ELEMENT_NODE:
+            if start:
+                text_content += get_text_content(child, start=True)
+            elif child.tagName == "div" and "css-1bm3l7o" in child.getAttribute("class").split():
+                start = True
+                text_content += get_text_content(child, start=True)
+    return text_content.strip()
+
 
 def dfs_search(element, class_names):
     if element.nodeType == element.ELEMENT_NODE:
@@ -25,9 +51,20 @@ def find_element_by_class_names(doc, tag_name, class_names):
     return elements
 
 
+def find_review_score(element):
+    if element.nodeType == element.ELEMENT_NODE and element.nodeName == "p":
+        class_attr = element.getAttribute('class')
+        if 'chakra-text' in class_attr.split() and 'css-7q120u' in class_attr.split():
+            return element.textContent.strip()
+    for child in element.childNodes:
+        result = find_review_score(child)
+        if result is not None:
+            return result
+    return None
+
+
 if xhtmlFile.startswith("downloaded_files/file1"):
     # parse for Michaels
-    print("Michaels")
     sql_store_name = "Michaels"
     dom = DOM.parse(xhtmlFile)
 
@@ -37,7 +74,7 @@ if xhtmlFile.startswith("downloaded_files/file1"):
         if elem.hasAttribute('class') and 'smallScreenTop' in elem.getAttribute('class'):
             h1_tag = elem.getElementsByTagName('h1')[0]
             sql_product_name = h1_tag.firstChild.nodeValue
-            print(sql_product_name)
+            # print(sql_product_name, "\n")
 
     #### EXTRACT PRICE ####
     meta_tags = dom.getElementsByTagName("meta")
@@ -46,7 +83,7 @@ if xhtmlFile.startswith("downloaded_files/file1"):
         if tag.hasAttribute("property") and tag.getAttribute("property") == "og:price:amount":
             sql_price = tag.getAttribute("content")
             break
-    print("Price:", sql_price)
+    # print("Price:", sql_price, "\n")
 
     #### EXTRACT IMAGE URL ####
     img_tags = dom.getElementsByTagName("img")
@@ -55,24 +92,89 @@ if xhtmlFile.startswith("downloaded_files/file1"):
     for tag in img_tags:
         img_count +=1
         if tag.hasAttribute("src") and img_count == 6:
-            image_url = tag.getAttribute("src")
+            sql_image_url = tag.getAttribute("src")
             break
-    print("Image URL:", image_url)
+    # print("Image URL:", sql_image_url, "\n")
 
 
 
-    #### EXTRACT REVIWS ####
+    #### EXTRACT REVIEWS ####
+    # div_elements = dom.getElementsByTagName('p')
+    # for div in div_elements:
+    #     class_name = div.getAttribute('class')
+    #     if class_name and ('chakra-text' in class_name.split() and 'css-1el49xh' in class_name.split()):
+    #         # Extract text nodes from the div element
+    #         text_content = ''.join(node.data.strip() for node in div.childNodes if node.nodeType == node.TEXT_NODE)
+    #         print(text_content)
+            # break
+    sql_review_score = None
 
+    #### EXTRACT DESCRIPTION ####
+    text_desc = find_element_by_id(dom.documentElement, 'description')
+
+    # Get the text content of the element
+    if text_desc:
+        sql_description = get_text_content(text_desc)
+        # print(sql_description)
+    else:
+        print("Description element not found.")
 
 else:
     # parse for Joann
-    print("Joann")
+    sql_store_name = "Joann"
+    dom = DOM.parse(xhtmlFile)
+
+    #### EXTRACT PRODUCT NAME ####
+    sql_product_name = dom.getElementsByTagName('h1')[0].childNodes[0].nodeValue.strip()
+    # print(sql_product_name)
+
+    #### EXTRACT PRICE ####
+    span_tags = dom.getElementsByTagName('span')
+    for span in span_tags:
+        if span.getAttribute('data-product-container') == 'sales-price':
+            sql_price = span.childNodes[0].nodeValue.strip()
+            # print(sql_price)
+            break  # Stop after finding the first matching <span> tag
+
+    #### EXTRACT IMAGE URL ####
+    img_tags = dom.getElementsByTagName('img')
+    for img in img_tags:
+        sql_image_url = img.getAttribute('src')
+        if sql_image_url.startswith('https://'):
+            # print(sql_image_url)
+            break
+
+    #### EXTRACT REVIEWS ####
+    span_elements = dom.getElementsByTagName('span')
+    for i, span in enumerate(span_elements, start=1):
+        if i == 32:
+            # Get the text content of the span element
+            sql_review_score = span.firstChild.nodeValue.strip()
+            # Check if sql_review_score is a number
+            try:
+                float(sql_review_score)
+            except ValueError:
+                sql_review_score = None
+            # print(sql_review_score)
+            break
+
+    #### EXTRACT DESCRIPTION ####
+    div_elements = dom.getElementsByTagName('div')
+    for div in div_elements:
+        class_name = div.getAttribute('class')
+        if class_name and ('cms-generic-copy' in class_name.split() and 'product__short-description' in class_name.split()):
+            # Extract text nodes from the div element
+            sql_description = ''.join(node.data.strip() for node in div.childNodes if node.nodeType == node.TEXT_NODE)
+            print(sql_description)
+            break  # Stop after finding the first matching div, remove this if you want to find all
+
+    if not sql_description:
+        sql_description = None
+        print(sql_description)
 
 
 
 
-
-'''
 #connect to server
 connection = mysql.connector.connect(
     host="localhost",
@@ -90,7 +192,7 @@ cursor.execute(
         description LONGTEXT,
         price FLOAT,
         image_url LONGTEXT,
-        review_score INTEGER
+        review_score INTEGER,
         store_name VARCHAR(25)
 
     )
@@ -107,15 +209,21 @@ cursor.execute(
     """
 )
 
-# also need to think about which store each item is from
-    #if file starts with file1... or file2...
-# loop through each file
-# using minidom grab info and store in variables
-# create sql insert function and insert into database
-# repeat loop
+
+#update products table
+cursor.execute(
+    """
+    INSERT INTO products288 (product_name, description, price, image_url, review_score, store_name)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """,
+    (sql_product_name, sql_description, sql_price, sql_image_url, sql_review_score, sql_store_name)
+)
+
+
+
+
 
 
 connection.commit()
 cursor.close()
 connection.close()
-'''
